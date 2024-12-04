225f24afb56e1fbd5dc1b637f4163690c627c320
Vlad Stoica <vladstoica@meta.com>
2024-12-04 11:36:40 +0200

    day 4

---
+++ 2024/day4/day4.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+from pathlib import Path
+import regex as re
+
+
+p = Path(__file__).with_name("input.txt")
+file = open(p)
+
+lines = [line.strip() for line in file.readlines()]
+
+di = [-1, -1, 0, 1, 1, 1, 0, -1]
+dj = [0, 1, 1, 1, 0, -1, -1, -1]
+# di = [0, 1, 1]
+# dj = [1, 1, 0]
+
+WORD = "XMAS"
+# WORD_REVERSED = "SAMX"
+
+rows = len(lines)
+cols = len(lines[0])
+
+print("Rows: ", rows, " Cols: ", cols)
+
+
+def count(i, j, distance, d, word):
+    if i < 0 or i >= rows or j < 0 or j >= cols:
+        return 0
+
+    if lines[i][j] != word[distance]:
+        return 0
+
+    if distance == len(word) - 1:
+        return 1
+
+    return count(i + di[d], j + dj[d], distance+1, d, word)
+
+
+def part1():
+    part1 = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            for d in range(len(di)):
+                part1 += count(i, j, 0, d, WORD)
+
+    return part1
+
+
+def part2():
+    part2 = 0
+    for i in range(rows - 2):
+        for j in range(cols - 2):
+            left = count(i, j, 0, 3, "MAS") + count(i, j, 0, 3, "SAM")
+            right = count(i, j + 2, 0, 5, "MAS") + count(i, j + 2, 0, 5, "SAM")
+
+            if left and right:
+                part2 += 1
+
+    return part2
+
+
+print("Part 1: ", part1())
+print("Part 2: ", part2())
