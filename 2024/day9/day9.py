80a12951444445a0bd09bebfff348f47efdba5a9
Vlad Stoica <vladstoica@meta.com>
2024-12-09 16:00:07 +0000

    attempt at improving

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -1,13 +1,6 @@
 from pathlib import Path
 
 
-def calculate_idx_sum(input):
-    idx_sum = [0]
-    for i in input:
-        idx_sum.append(idx_sum[-1] + abs(i[0]))
-    return idx_sum
-
-
 def parse():
     file = open(Path(__file__).with_name("input.txt"))
     input = [int(x) for x in file.readline().strip()]
@@ -29,31 +22,64 @@ def sum_f(start, end):
     return ((end - start + 1) * (end + start)) // 2
 
 
+def print_input(input):
+    print('---')
+    print(input)
+    for (c, id) in input:
+        for x in range(abs(c)):
+            print(id if id >= 0 else '.', end="")
+    print("")
+
+
 def calculate(part2):
     input = parse()
 
     result = 0
     start = 0
 
-    while start < len(input):
-        if input[start][0] < 0:
-            end = len(input) - 1
-            while input[end][0] <= 0 or (part2 and input[end][0] > abs(input[start][0])) and end >= start:
-                end -= 1
+    end = len(input) - 1
 
-            if end < 0 or end < start:
+    while end >= 0:
+
+        if input[end][0] >= 0:
+            start = 0
+            while (input[start][0] >= 0 or (part2 and abs(input[start][0]) < input[end][0])) and end > start:
                 start += 1
+
+            if end <= start:
+                end -= 1
                 continue
 
             to_add = min(input[end][0], abs(input[start][0]))
 
-            input[end] = modify(input[end], -to_add)
-            id = input[end][1]
-            input.insert(end, (-to_add, -1))
-            input[start] = modify(input[start], to_add)
-            input.insert(start, (to_add, id))
+            if to_add == 0:
+                end -= 1
+                continue
 
-        start += 1
+            id = input[end][1]
+
+            input[start] = modify(input[start], to_add)
+
+            if input[start] == 0:
+                del input[start]
+                end -= 1
+
+            input.insert(start, (to_add, id))
+            end += 1
+
+            input[end] = modify(input[end], -to_add)
+            if input[end][0] == 0:
+                del input[end]
+                end -= 1
+
+            input.insert(end + 1, (-to_add, -1))
+            end += 1
+            while end < len(input) and input[end - 1][0] < 0 and input[end][0] < 0:
+                input[end - 1] = modify(input[end - 1], input[end][0])
+                del input[end]
+
+        end -= 1
+
     id_so_far = 0
 
     for (c, id) in input:
