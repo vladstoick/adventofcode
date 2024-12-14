67b197e0d849328dae4ffd9137f7f10324264736
Vlad Stoica <vladstoica@meta.com>
2024-12-14 10:53:30 +0000

    day12

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 file = open(Path(__file__).with_name("input.txt"))
@@ -9,11 +10,10 @@ di = [-1, 0, 1, 0]
 dj = [0, 1, 0, -1]
 
 visited = set()
+edges = defaultdict(lambda: [])
 
 
-def depth(i, j, d, orig):
-    if i < 0 or i == rows or j < 0 or j == cols or lines[i][j] != orig:
-        return (0, 1)
+def depth(i, j, orig):
 
     if (i, j) in visited:
         return (0, 0)
@@ -26,24 +26,41 @@ def depth(i, j, d, orig):
     for d in range(len(di)):
         ni = i + di[d]
         nj = j + dj[d]
-        a, p = depth(ni, nj, orig)
-        area += a
-        perim += p
+
+        if ni < 0 or ni == rows or nj < 0 or nj == cols or lines[ni][nj] != orig:
+            perim += 1
+            edges[(i, j)].append(d)
+        else:
+            a, p = depth(ni, nj, orig)
+            area += a
+            perim += p
 
     return (area, perim)
 
 
 part1 = 0
+part2 = 0
 
 for i in range(rows):
     for j in range(cols):
         if (i, j) not in visited:
+            edges = defaultdict(lambda: [])
             a, p = depth(i, j, lines[i][j])
             part1 += a * p
 
-# part1 = 0
+            e = 0
 
-# for k in area_calc.keys():
-#     part1 += area_calc[k] * perim_calc[k]
+            keys = list(edges.keys())
+            for (ei, ej) in keys:
+                for d in range(4):
+                    if d in edges[(ei, ej)]:
+                        ni, nj = ei + di[(d - 1) % 4], ej + dj[(d - 1) % 4]
+
+                        if d not in edges[(ni, nj)]:
+                            e += 1
+
+            part2 += a * e
+
 
 print("Part 1", part1)
+print("Part 2", part2)
